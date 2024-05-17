b2251428f50ee780f41508e51ca181d3114ead40
Alok Jha <axjh03@gmail.com>
2024-05-17 18:16:41 -0500

    scroll and navbar

--- navbar.js
+++ navbar.js
@@ -1,3 +1,5 @@
-window.addEventListener("scroll", function() {
-    
-}
+// Change the navbar color when scrolling
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('header');
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+})
