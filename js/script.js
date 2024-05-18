141db1ffdfe91ae217f8de3853bc310c0a1b9c32
Alok Jha <axjh03@gmail.com>
2024-05-18 16:13:02 -0500

    about section complete

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('resume-button').addEventListener('click', function() {
+        window.location.href = "https://www.linkedin.com/in/axjh03/";
+    });
+});
