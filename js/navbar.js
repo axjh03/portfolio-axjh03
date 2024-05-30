59272157e4bd6ed086e75b243ba13f60eb067508
Alok Jha <axjh03>
2024-05-30 01:20:18 -0500

    pc

--- js/navbar.js
+++ js/navbar.js
@@ -3,3 +3,37 @@ window.addEventListener('scroll', function() {
     var navbar = document.querySelector('header');
     navbar.classList.toggle('sticky', window.scrollY > 0);
 })
+
+// Function to toggle headers based on screen size
+function toggleHeaders() {
+    const desktopHeader = document.getElementById('desktop');
+    const phoneHeader = document.getElementById('phone');
+  
+    if (window.innerWidth <= 768) {
+      desktopHeader.classList.add('hidden');
+      desktopHeader.classList.remove('show');
+      phoneHeader.classList.add('show');
+      phoneHeader.classList.remove('hidden');
+    } else {
+      desktopHeader.classList.add('show');
+      desktopHeader.classList.remove('hidden');
+      phoneHeader.classList.add('hidden');
+      phoneHeader.classList.remove('show');
+    }
+  }
+  
+  // Add event listener for window resize
+  window.addEventListener('resize', toggleHeaders);
+  
+  // Call the function initially to set the correct header on page load
+  toggleHeaders();
+  
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
