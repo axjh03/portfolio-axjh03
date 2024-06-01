e6a1b87d94ee1c8a18243d0f2934599a9418a338
Alok Jha <axjh03@gmail.com>
2024-05-31 21:11:32 -0500

    blog responsive

--- js/navbar.js
+++ js/navbar.js
@@ -31,9 +31,19 @@ function toggleHeaders() {
   /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
+    var y = document.getElementById("username-phone");
     if (x.style.display === "block") {
       x.style.display = "none";
+      y.style.display = "block";
+      y.style.color = "aqua";
     } else {
       x.style.display = "block";
+      y.style.display = "none";
+      y.style.color = "aqua";
     }
   }
+
+
+
+
+  // on click of burger-button id make id 'username' class set to "hidden"
