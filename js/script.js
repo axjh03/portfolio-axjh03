e85deec9638d86ab836e8e3f3adb8e1c1d87ade3
Alok Jha <axjh03@gmail.com>
2024-07-09 14:10:10 -0500

    Added resume

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('resume-button').addEventListener('click', function() {
-        window.location.href = "https://www.linkedin.com/in/axjh03/";
+        window.location.href = "https://drive.google.com/file/d/1JNponY8gTv1U9vU80yv28xJV8v1dBZS-/view?usp=sharing";
     });
 });
+
+
