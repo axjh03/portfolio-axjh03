78672ed949272e6dd252919c23264c9ea2e82c98
Alok Jha <axjh03@gmail.com>
2024-10-10 15:44:50 -0500

    updated resume link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('resume-button').addEventListener('click', function() {
-        window.location.href = "https://drive.google.com/file/d/1JNponY8gTv1U9vU80yv28xJV8v1dBZS-/view?usp=sharing";
+        window.location.href = "https://drive.google.com/file/d/11DuKsmZNdSLVsnNVs3TzLbVVNQEUiEEU/view?usp=sharing";
     });
 });
 
