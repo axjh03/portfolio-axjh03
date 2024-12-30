058c7d81bd909a0cc57e714375752e5da805d7e8
Alok Jha <axjh03@gmail.com>
2024-12-30 14:07:41 -0600

    v2.0 starting up

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('resume-button').addEventListener('click', function() {
-        window.location.href = "https://drive.google.com/file/d/11DuKsmZNdSLVsnNVs3TzLbVVNQEUiEEU/view?usp=sharing";
-    });
-});
-
-
