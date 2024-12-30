2de3b326e989ad6bd2a0f43d8b5cdf686f3e7c61
Alok Jha <axjh03@gmail.com>
2024-12-30 14:52:24 -0600

    navbar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import {DateRangePicker} from "@nextui-org/react";
+import "../index.css";
+
+
+export default function Footer() {
+  return <DateRangePicker className="max-w-xs" label="Stay duration" />;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const {nextui} = require('@nextui-org/theme');
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/react/**/*.{js,jsx,ts,tsx}",
+    "// Include NextUI components",
+    "./node_modules/@nextui-org/theme/dist/components/(date-picker|dropdown|navbar|button|ripple|spinner|calendar|date-input|form|popover|menu|divider).js"
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
   plugins: [nextui()],
-}
-
+};
