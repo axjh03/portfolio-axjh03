5d174c632fe4634fb1b4cf679201145ae5b26813
Alok Jha <axjh03@gmail.com>
2025-05-15 01:06:00 -0500

    .

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faReact,
   faDocker,
-  faPython,
 } from "@fortawesome/free-brands-svg-icons";
 import Chip from "@mui/material/Chip";
 import "../assets/styles/Expertise.scss";
