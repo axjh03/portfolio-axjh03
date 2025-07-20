a3885103be6c538673bb78c878ddf69bcb12a50b
Alok Jha <axjh03@gmail.com>
2025-07-19 21:47:19 -0500

    updated link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -36,7 +36,7 @@ export const projects: Project[] = [
       "Render", 
       "Netlify"
     ],
-    demoUrl: "https://qalam-axjh03.netlify.app/",
+    demoUrl: "https://rad-truffle-6266c3.netlify.app/",
     sourceUrl: "https://github.com/axjh03/Qalam",
   },
   {
