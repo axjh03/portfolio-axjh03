a018ba1ea8ccf48ab453a559989a00783437613e
Alok Jha <axjh03@gmail.com>
2025-09-03 09:54:54 -0500

    updated resume

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -30,7 +30,7 @@ function Main({ parentToChild }: any) {
           
           <button 
             className="resume-button"
-            onClick={() => window.open('https://drive.google.com/file/d/1FjKmV_QoJElCulB75vVfW8MdaM2UtFhB/view?usp=sharing', '_blank')}
+            onClick={() => window.open('https://drive.google.com/file/d/17dQRuHXyPVkD4dXMAsFBrU68HZzx72W3/view?usp=sharing', '_blank')}
             style={{
               backgroundColor: isDark ? 'rgba(15, 23, 42, 0.25)' : 'rgba(255, 255, 255, 0.25)',
               backdropFilter: 'blur(10px)',
